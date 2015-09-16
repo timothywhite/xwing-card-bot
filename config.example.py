05e9bb40742d42c8f223fa33a68e75257379fea0
Tim White <timford88@gmail.com>
2015-09-16 20:33:51 +0000

    add example config

---
+++ config.example.py
@@ -0,0 +1,9 @@
+#populate with correct values and rename to config.py before running the bot
+user_agent = 'Unique Identifying String'
+subreddit = 'the target subreddit'
+client_id = 'Reddit OAuth2 client id'
+client_secret ='Reddit OAuth2 client secret'
+redirect_uri = 'the redirect uri in your Reddit OAuth2 app config'
+refresh_token = 'refresh token for the inteded bot user'
+post_limit = 10 #number of hot posts to parse
+api_base_url = 'baseurl for the xwing api' #see https://github.com/timothywhite/xwsb-api
