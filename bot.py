75a38253079a02c431cc7a5488dd98ed1114348f
Tim White <timford88@gmail.com>
2015-09-16 22:52:15 +0000

    return all possible cards when tag is ambiguous

--- bot.py
+++ bot.py
@@ -103,21 +103,32 @@ class XWingTMGCardBot:
         else:
             raise TypeException('Object not submission or comment')
 
+    def get_cards(self, tags):
+        cards = [self.api.get_card(t) for t in tags] #get cards
+        cards = [c for ca in cards for c in ca] #flatten card list
+        cards = [i for n,i in enumerate(cards) if i not in cards[n+1:]] #remove duplicates
+        cards = sorted(cards, key=lambda c: c['name']) #sort by name
+
+        return cards
+
     def build_comment(self, post):
         comment = ''
         tags = self.parse_text(self.get_text(post))
+        cards = self.get_cards(tags)
 
-        for index, tag in enumerate(tags):
-            card = self.api.get_card(tag)
-            if card:
-                comment += self.render_card(card)
-            if index != len(tags) - 1:
+        for index, card in enumerate(cards):
+            comment += self.render_card(card)
+            if index != len(cards) - 1:
                 comment += '\n\n&nbsp;\n\n---\n\n&nbsp;\n\n'
+
         if comment != '':
             comment += '\n\n&nbsp;\n\n Am I popping too much Glitterstim? Message /u/forkmonkey88'
 
         return comment
+
     def post_comment(self, obj, comment):
+        print comment
+        return
         if type(obj) == praw.objects.Comment:
             return obj.reply(comment)
         elif type(obj) == praw.objects.Submission:
@@ -127,22 +138,22 @@ class XWingTMGCardBot:
 
     def mash_go(self):
         for post in self.posts:
-            try:
-                if not self.replied_to(post):
-                    comment = self.build_comment(post)
-                    if comment:
-                        self.post_comment(post, comment)
-            except Exception:
-                print 'Unable to process post: ' + str(post)
+            #try:
+            if not self.replied_to(post):
+                comment = self.build_comment(post)
+                if comment:
+                    self.post_comment(post, comment)
+            #except Exception:
+            #    print 'Unable to process post: ' + str(post)
             for comment in praw.helpers.flatten_tree(post.comments):
-                try:
+                #try:
                     if not self.replied_to(comment) and not self.own_comment(comment):
                         reply = self.build_comment(comment)
                         if reply:
                             self.post_comment(comment, reply)
-                except Exception:
-                    print 'Unable to process comment: ' + str(comment)
-            
+                #except Exception:
+                #    print 'Unable to process comment: ' + str(comment)
+
 if __name__ == '__main__':
     bot = XWingTMGCardBot(config)
     bot.mash_go()
--- xwingapi.py
+++ xwingapi.py
@@ -6,7 +6,7 @@ class XWingAPI:
         self.cache = {}
         self.cards = []
         self.base_url = base_url
-    
+
     def get_card(self, tag):
         filters = {
             'type': ('get_upgrade_type', 'name'),
@@ -14,76 +14,75 @@ class XWingAPI:
         }
         cards = filter(lambda c: self.compare(tag['card'], c), self.get_cards())
         filtered_cards = cards
-        if len(cards) > 0:
-            for filter_type, (getter, cmp_prop) in filters.iteritems():
-                if tag[filter_type]:
-                    def filter_func(c):
-                        if filter_type in c:
-                            obj = None
-                            if type(c[filter_type]) == dict:
-                                obj = c[filter_type]
-                            else:
-                                obj = getattr(self, getter)(c[filter_type])
-                            if obj:
-                                return self.compare(tag[filter_type], obj, cmp_prop)
-                        
-                        return False
-                    
-                    filtered_cards = filter(filter_func, filtered_cards)
-            if len(filtered_cards) > 0:
-                return filtered_cards[0]
+
+        for filter_type, (getter, cmp_prop) in filters.iteritems():
+            if tag[filter_type]:
+                def filter_func(c):
+                    if filter_type in c:
+                        obj = None
+                        if type(c[filter_type]) == dict:
+                            obj = c[filter_type]
+                        else:
+                            obj = getattr(self, getter)(c[filter_type])
+                        if obj:
+                            return self.compare(tag[filter_type], obj, cmp_prop)
+
+                    return False
+
+                filtered_cards = filter(filter_func, filtered_cards)
             
-            return cards[0]
-        else:
-            return False
-    
+        if len(filtered_cards) > 0:
+            return filtered_cards
+
+        return cards
+
     def get_cards(self):
         if len(self.cards) == 0:
             self.cards = self.get_pilots() + self.get_upgrades()
         return self.cards
-        
+
     def get_objs(self, obj_type, url = None):
         url = url if url else obj_type
         if obj_type not in self.cache:
             r = requests.get(self.base_url + url)
             self.cache[obj_type] = r.json()
         return self.cache[obj_type]
-        
+
     def get_pilots(self):
         return self.get_objs('pilot')
-    
+
     def get_ships(self):
         return self.get_objs('ship')
-    
+
     def get_ship(self, id):
         ships = filter(lambda s: s['id'] == id, self.get_ships())
         if len(ships) > 0:
             return ships[0]
         else:
             return False
-        
+
     def get_upgrade_types(self):
         return self.get_objs('upgrade/type')
-    
+
     def get_upgrade_type(self, id):
         types = filter(lambda s: s['id'] == id, self.get_upgrade_types())
         if len(types) > 0:
             return types[0]
         else:
             return False
-        
+
     def get_factions(self):
         return self.get_objs('faction')
-        
+
     def get_faction(self, id):
         factions = filter(lambda f: f['id'] == id, self.get_factions())
         if len(factions) > 0:
             return factions[0]
         else:
             return False
-        
+
     def get_upgrades(self):
         return self.get_objs('upgrade')
-    
+
     def compare(self, name, card, prop = 'name'):
         return card[prop].lower() == name.lower()
